56ec54097c7cacc41507f4860287608fd32487f4
somtamas <somlyaitamas@turr.hu>
2023-12-13 10:25:55 +0100

    sima szorzás

--- szamolasok.py
+++ szamolasok.py
@@ -7,5 +7,11 @@ print(osszeadasmeg)
 #kivonás
 beker3=int(input("Kérlek, adj meg egy számot!"))
 beker4=int(input("Kérlek, adj meg egy számot!"))
-kivonasmeg=beker3+beker4
+kivonasmeg=beker3-beker4
 print(kivonasmeg)
+
+#szorzás
+beker5=int(input("Kérlek, adj meg egy számot!"))
+beker6=int(input("Kérlek, adj meg egy számot!"))
+szorzasmeg=beker5*beker6
+print(szorzasmeg)
