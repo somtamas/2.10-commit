afdfc292fff80d37d4ddae90d4023dc9a75db47c
somtamas <somlyaitamas@turr.hu>
2023-12-13 10:24:24 +0100

    sima kivonás

--- szamolasok.py
+++ szamolasok.py
@@ -4,3 +4,8 @@ beker2=int(input("Kérlek, adj meg egy számot!"))
 osszeadasmeg=beker1+beker2
 print(osszeadasmeg)
 
+#kivonás
+beker3=int(input("Kérlek, adj meg egy számot!"))
+beker4=int(input("Kérlek, adj meg egy számot!"))
+kivonasmeg=beker3+beker4
+print(kivonasmeg)
